50d7fb8236731e86f7f0dc6cffd732ca578b2beb
RealZ <12774452+ReakZ@users.noreply.github.com>
2021-03-14 10:21:52 +0300

    feat: implement min function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
+exports.min = function min(array) {
+    if (array) {
+        let min = array.length ? array[0] : 0;
+        for (let index = 0; index < array.length; index++) {
+            if (array[index] < min) {
+                min = array[index];
+            }
+        }
+        return min;
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+        if (array) {
+        let min = array.length ? array[0] : 0;
+        for (let index = 0; index < array.length; index++) {
+            if (array[index] < min) {
+                min = array[index];
+            }
+        }
+        return min;
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return 0;
+};
